51ac4d2073cdb6699f64823fcdd0d5e7bb3bdeb2
kanishkrawat17 <kanishkrawat67@gmail.com>
2024-01-07 18:52:51 +0530

    implented getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,33 @@
 function HomePage(props) {
+  const { products, initData } = props;
+  console.log("homapeage", initData);
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.name}</li>
+      ))}
     </ul>
   );
 }
+export async function getStaticProps() {
+  let initData = [];
 
+  const fetchData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+    const data = await response.json();
+    initData = [...data];
+  };
+  
+  await fetchData();
+
+  return {
+    props: {
+      products: [
+        { id: 1, name: "Product 1" },
+        { id: 2, name: "Product 2" },
+      ],
+      initData,
+    },
+  };
+}
 export default HomePage;
